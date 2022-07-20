8d1b9a536b47a0102926663bfad75f412ba75679
freedomchurl <freedomchurl@naver.com>
2022-07-21 02:01:40 +0900

    [DEV] json property 설정 추가

--- web/dto/commentDTO.go
+++ web/dto/commentDTO.go
@@ -10,8 +10,8 @@ Comment 를 저장하기 위한 구조체
 - 접근제어지시자가 대/소문자로 구분된다고 봐야 함
 */
 type CommentDTO struct {
-	AlbumSeq int64
-	Content  string
+	AlbumSeq int64  `json:"albumSeq"`
+	Content  string `json:"content"`
 }
 
 // 화살표의 방향이 안으로 흐르기 위해서는, DTO가 Entity를 바라보고 변환해야 함
