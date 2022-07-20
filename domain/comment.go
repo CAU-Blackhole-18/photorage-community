7dff7b38c0a9e5a7317bd29372f19e042f476fad
freedomchurl <freedomchurl@naver.com>
2022-07-21 01:50:28 +0900

    [DEV] Comment Entity 생성 및 간단한 인터페이스/메서드 추가

--- domain/comment.go
+++ domain/comment.go
@@ -1,8 +1,25 @@
 package domain
 
+import "fmt"
+
 /*
-Comment 를 저장하기 위한 구조체
+Comment Entity를 표현하기 위한 구조체
 */
 type Comment struct {
-	albumSeq int64
+	AlbumSeq int64
+	Content  string
+}
+
+// Comment에 Bind(Receiver)를 붙인 Method
+func (c *Comment) PrintCommentInfo() {
+	fmt.Println("My Comment Info = ", c.AlbumSeq, c.Content)
+}
+
+func (c Comment) Run() int32 {
+	fmt.Println("Interface = ", c.AlbumSeq, c.Content)
+	return int32(c.AlbumSeq)
+}
+
+type Animal interface {
+	Run() int32
 }
